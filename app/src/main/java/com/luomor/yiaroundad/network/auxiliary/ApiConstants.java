5a348d1fd004fb3d8aa16cfdf1b4d3f6fe368b0b
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-08-22 17:54:12 +0800

    1.新功能:增加附近美食店铺功能

--- app/src/main/java/com/luomor/yiaroundad/network/auxiliary/ApiConstants.java
+++ app/src/main/java/com/luomor/yiaroundad/network/auxiliary/ApiConstants.java
@@ -8,17 +8,17 @@ package com.luomor.yiaroundad.network.auxiliary;
  */
 
 public class ApiConstants {
-    public static final String BILI_GO_BASE_URL = "http://bilibili-service.daoapp.io/";
-    public static final String RANK_BASE_URL = "http://www.bilibili.com/";
-    public static final String APP_BASE_URL = "http://app.bilibili.com/";
-    public static final String LIVE_BASE_URL = "http://live.bilibili.com/";
-    public static final String API_BASE_URL = "http://api.bilibili.cn/";
-    public static final String FOOD_BASE_URL = "http://bangumi.bilibili.com/";
-    public static final String SEARCH_BASE_URL = "http://s.search.bilibili.com/";
+    public static final String BILI_GO_BASE_URL = "https://bilibili-service.daoapp.io/";
+    public static final String RANK_BASE_URL = "https://www.bilibili.com/";
+    public static final String APP_BASE_URL = "https://app.bilibili.com/";
+    public static final String LIVE_BASE_URL = "https://live.bilibili.com/";
+    public static final String API_BASE_URL = "https://api.bilibili.cn/";
+    public static final String FOOD_BASE_URL = "https://bangumi.bilibili.com/";
+    public static final String SEARCH_BASE_URL = "https://s.search.bilibili.com/";
     public static final String ACCOUNT_BASE_URL = "https://account.bilibili.com/";
-    public static final String USER_BASE_URL = "http://space.bilibili.com/";
-    public static final String VIP_BASE_URL = "http://vip.bilibili.com/";
-    public static final String IM9_BASE_URL = "http://www.im9.com/";
+    public static final String USER_BASE_URL = "https://space.bilibili.com/";
+    public static final String VIP_BASE_URL = "https://vip.bilibili.com/";
+    public static final String IM9_BASE_URL = "https://www.im9.com/";
 
     public static final String AROUND_BASE_URL = "https://around.luomor.com/";
 
