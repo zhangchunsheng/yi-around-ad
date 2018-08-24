bec29ff8ebc8ca9092efd1c9166224b1315643a4
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-08-24 16:10:27 +0800

    4.新功能:增加热词功能

--- app/src/main/java/com/luomor/yiaroundad/network/around/AdAppService.java
+++ app/src/main/java/com/luomor/yiaroundad/network/around/AdAppService.java
@@ -2,8 +2,10 @@ package com.luomor.yiaroundad.network.around;
 
 import com.luomor.yiaroundad.entity.recommend.RecommendBannerInfo;
 import com.luomor.yiaroundad.entity.recommend.RecommendInfo;
+import com.luomor.yiaroundad.entity.search.SearchArchiveInfo;
 
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 
 /**
@@ -22,4 +24,11 @@ public interface AdAppService {
      */
     @GET("recommend/getBanners?version=1")
     Observable<RecommendBannerInfo> getRecommendedBannerInfo();
+
+    /**
+     * 综合搜索
+     */
+    @GET("x/v2/search?actionKey=appkey&appkey=27eb53fc9058f8c3&build=3710&device=phone&duration=0&mobi_app=iphone&order=default&platform=ios&rid=0")
+    Observable<SearchArchiveInfo> searchArchive(
+            @Query("keyword") String content, @Query("pn") int page, @Query("ps") int pagesize);
 }
